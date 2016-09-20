40240a7f79e1c381f495e7155d7cc4703aa0f355
Dorin Ionita <dorin.marian.ionita@gmail.com>
2016-09-20 20:22:07 +0300

    added_condition_not_to_duplicate_name_or_URL_in_file

--- initGit.sh
+++ initGit.sh
@@ -6,9 +6,11 @@
 echo "----Making directory to store introduced data..."
 mkdir Gitinfo
 echo "----Saving the name of the repository (used locally)..."
-echo $1 >> ./Gitinfo/Repo_name.txt
+[ -f ./Gitinfo/Repo_name.txt ] && echo $1 >> ./Gitinfo/Repo_name.txt ||
+	echo "There was another name for the repo allready saved to the file."
 echo "----Saving the GitHub URL of the repository..."
-echo $2 >> ./Gitinfo/Repo_url.txt
+[-f ./Gitinfo/Repo_name.txt ] && echo $2 >> ./Gitinfo/Repo_url.txt ||
+	echo "There was another URL for the repo allready saved to the file."
 
 echo "---Starting initialization..."
 git init
