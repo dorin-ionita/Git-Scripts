5e50944bd8a6c28799f5254b212da873575b7e18
Dorin Ionita <dorin.marian.ionita@gmail.com>
2016-09-20 20:12:52 +0300

    first_commit,_initGit.sh_added_but_not_tested

---
+++ updateGit.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "----Adding files..."
+git add Readme.txt
+git add graphicInterface.py
+git add main.py
+git add mySqlDBInterface.py
+git add scheduler.py
+git add updateGitHub.sh
+git add ./cfg/start_date.cfg
+echo "----Making the commit $1..."
+git commit -m $1
+echo "----Pushing..."
+git push origin master
+# cum rezolv faza cu credentialele...?
+echo "----Git Hub updated succesfully!"
